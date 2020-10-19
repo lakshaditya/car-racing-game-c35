adabdc3bfcf2038303383cf24a5ed0955e21498c
lakshaditya <70273436+lakshaditya@users.noreply.github.com>
2020-10-19 19:10:51 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,26 @@
+class Player {
+constructor(){}
+
+getCount(){
+    var playerCountref = database.ref('playerCount');
+    playerCountref.on("value",function(data){
+        playerCount = data.val();
+    }) 
+
+}
+updateCount(count){
+    database.ref('/').update({
+        playerCount:count
+    })
+}
+update(name){
+    var Playerindex = "Player"+playerCount;
+    database.ref(Playerindex).set({
+        name:name
+    });
+
+    
+}
+
+}
+
